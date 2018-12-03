d34cca3343924e5371a449368f1def34deb98a90
Funday Pande <fundaylogin@gmail.com>
2018-12-03 16:49:06 +0800

    sql upload

--- wisni2.sql
+++ wisni2.sql
@@ -2,10 +2,10 @@
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 03, 2018 at 04:48 PM
--- Server version: 10.1.35-MariaDB
--- PHP Version: 7.2.9
+-- Host: localhost
+-- Generation Time: Dec 03, 2018 at 09:44 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.0.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,34 +25,57 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `gambar`
+--
+
+CREATE TABLE `gambar` (
+  `id_gambar` int(11) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `keterangan` text NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tpengolahan`
+--
+
+CREATE TABLE `tpengolahan` (
+  `id_pengolahan` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `keterangan` text NOT NULL,
+  `id_tanaman` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tpengolahan`
+--
+
+INSERT INTO `tpengolahan` (`id_pengolahan`, `nama`, `keterangan`, `id_tanaman`) VALUES
+(23, 'Tumbuk', 'Tumbuk aja terus', 22),
+(24, 'Bejek', 'Bejek terus', 22);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `ttanaman`
 --
 
 CREATE TABLE `ttanaman` (
   `id_tanaman` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
-  `keterangan` text NOT NULL
+  `keterangan` text NOT NULL,
+  `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `ttanaman`
 --
 
-INSERT INTO `ttanaman` (`id_tanaman`, `nama`, `keterangan`) VALUES
-(1, 'Bayam', 'bayam adalahakjfnadf'),
-(2, 'Pande', '2018-12-12'),
-(3, 'Buncis', 'Buncis adalah'),
-(4, 'Pinahun', 'Hebat'),
-(5, 'Pinahun', 'Hebat'),
-(6, 'Kucing', 'Ayam keterangan'),
-(7, 'Kumis Kucing', 'Keterangan Kumis Kuing'),
-(8, 'Kumis Kucing', 'Keterangan Kumis Kuing'),
-(9, 'Kumis Kucing', 'Keterangan Kumis Kuing'),
-(10, 'Kumis Kucing', 'Keterangan Kumis Kuing'),
-(11, 'Kumis Kucing', 'Keterangan Kumis Kuing'),
-(12, 'Kumis Kucing', 'Keterangan Kumis Kuing'),
-(13, 'Kumis Kucing', 'Keterangan Kumis Kuing'),
-(14, 'Alonsi', 'Keteranganya');
+INSERT INTO `ttanaman` (`id_tanaman`, `nama`, `keterangan`, `id`) VALUES
+(21, 'Kumis Kucing', 'Di Tempat Kering', 5),
+(22, 'Cocor Bebek', 'Budidayanya di tanah basah', 5);
 
 -- --------------------------------------------------------
 
@@ -86,10 +109,25 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `name`, `photo`, `ro
 --
 
 --
+-- Indexes for table `gambar`
+--
+ALTER TABLE `gambar`
+  ADD PRIMARY KEY (`id_gambar`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `tpengolahan`
+--
+ALTER TABLE `tpengolahan`
+  ADD PRIMARY KEY (`id_pengolahan`),
+  ADD KEY `id_tanaman` (`id_tanaman`);
+
+--
 -- Indexes for table `ttanaman`
 --
 ALTER TABLE `ttanaman`
-  ADD PRIMARY KEY (`id_tanaman`);
+  ADD PRIMARY KEY (`id_tanaman`),
+  ADD KEY `id_user` (`id`);
 
 --
 -- Indexes for table `users`
@@ -103,16 +141,44 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `gambar`
+--
+ALTER TABLE `gambar`
+  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tpengolahan`
+--
+ALTER TABLE `tpengolahan`
+  MODIFY `id_pengolahan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
 -- AUTO_INCREMENT for table `ttanaman`
 --
 ALTER TABLE `ttanaman`
-  MODIFY `id_tanaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_tanaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tpengolahan`
+--
+ALTER TABLE `tpengolahan`
+  ADD CONSTRAINT `tpengolahan_ibfk_1` FOREIGN KEY (`id_tanaman`) REFERENCES `ttanaman` (`id_tanaman`);
+
+--
+-- Constraints for table `ttanaman`
+--
+ALTER TABLE `ttanaman`
+  ADD CONSTRAINT `ttanaman_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
